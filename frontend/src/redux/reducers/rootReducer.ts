efbbd48364c42457cbd55307c125c95773058402
Vanduc19it <duc19it@gmail.com>
2024-02-19 09:01:47 +0700

    Init

--- frontend/src/redux/reducers/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import userReducer from "./userReducer";
-import chatReducer from "./chatReducer";
-
-export default combineReducers({
-  userReducer,
-  chatReducer,
-});
--- frontend/src/utils/stringUtils.ts
+++
@@ -1,5 +0,0 @@
-export function getInitials(str: string): string {
-  const words = str.split(" ");
-  const initials = words.map((word) => word.charAt(0));
-  return initials.join("");
-}
